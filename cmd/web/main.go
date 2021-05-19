3bf75efc6e82f31ed9346bea704ba96cc1c376d8
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2021-05-19 23:39:35 +0000

    go websocket app

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/peterzhuang/go-websocket/internal/handlers"
+)
+
+func main() {
+	mux := routes()
+
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
+
+	log.Println("Starting web server on port 8080")
+
+	_ = http.ListenAndServe(":8080", mux)
+
+}
